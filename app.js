ac223d16ca291e7c4d302fceb3d3441ae1e1993c
Bunks18 <wbunker11@gmail.com>
2017-01-03 22:25:14 -0500

    updated for number ex

--- app.js
+++ app.js
@@ -15,9 +15,32 @@ function textNormalizer(text) {
   return stripEnd;
 };
 
+function computeArea(width, height) {
+  return width * height;
+  // your code here
+};
+
+function isDivisible(divisee, divisor) {
+   return divisee % divisor === 0;
+};
+
+function celsToFahr(celsTemp) {
+  // your code here
+  return (celsTemp * (9/5)) + 32;
+
+}
+
+function fahrToCels(fahrTemp) {
+  return ((fahrTemp -32)) * (5/9);
+  //[°F] = [°C] ×  9⁄5 + 32
+  // your code here
+}
+
 console.log((wisePerson("Will", "Y0")));
 
 console.log((textNormalizer("YOOOO   ")));
+
+
 
 
 // function shouter(whatToShout) {
